729f37f4684e1bc1c328bc9dc14fe22eb3cdcca8
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-02-28 22:28:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// Append new list
+
 function sample() {
 	var textInput = document.getElementById("input-text").value;
-	listNode = document.getElementById("list");
+	var list = document.createElement("li");
+	var node = document.createTextNode(textInput);
+	var listNode = document.getElementById("ul-list");
+	var before = document.getElementById("li1");
+
+	list.appendChild(node);
+	listNode.insertBefore(list, before);
+
+	var button = document.createElement("button");
+//	button.innerText = 'Delete';
+	list.appendChild(button);
 }
+
+/* 
+*/
+
+
