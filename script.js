844b3ccc4f8c8e0bd6bde45ac3ee81f0267a1974
Allyn <44602686+allynkalda@users.noreply.github.com>
2019-03-27 14:04:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
-// Append new list
+// Create random word generator
+var wordArray = [];
+var randomWords = [ "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "Dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "Ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "memento", "oxygen", "pajama", "Pixel", "zombie", "tomato", "shark", "elephant", "island",]
 
-function sample() {
-	var textInput = document.getElementById("input-text").value;
-	var list = document.createElement("li");
-	var node = document.createTextNode(textInput);
-	var listNode = document.getElementById("ul-list");
-	var before = document.getElementById("li1");
+var number = Math.floor((Math.random() * 29) + 1);
+var word = randomWords[number];
+// Start function picks random word
 
-	list.appendChild(node);
-	listNode.insertBefore(list, before);
+function start() {
+	length();
 
-	var button = document.createElement("button");
-//	button.innerText = 'Delete';
-	list.appendChild(button);
+// Calculates length of word
+
+	function length() { 
+		var printLetter = word.length;
+		// Prints out number of letters
+		console.log(printLetter);
+		var parent = document.querySelector("ul");
+		// Push each letter into an array
+		for (var i = 0; i < word.length; i++) {
+			wordArray.push(word[i]);
+		// Visualize the letters of the words
+			var h = document.createElement("li");
+			h.setAttribute("id", i);              
+			var t = document.createTextNode("__"); 
+			h.appendChild(t);
+			document.body.insertBefore(h, parent);
+		}
+		console.log(wordArray);
+	}
+
+
 }
 
-/* 
-*/
 
+// Receives letter input from player
+var counter = 0;
 
+function guess(input) {
+	counter++;
+	var input;
+	//= document.getElementsByClassName("letter");
+		// Loop if input letter is same as letters in array
+		for (var j = 0; j < word.length; j++) {
+			if (input == word[j]) {
+				// Change appended list to show letter
+				var modified = document.getElementById(j);
+				modified.textContent = word[j];
+				//parent.replaceChild(modified, parent.childNodes[j]);
+				console.log("yep");
+			} else {
+				console.log("nope");		
+			}
+		}
+	console.log(input);
+	console.log(counter);
+}
